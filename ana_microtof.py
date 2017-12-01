4a34d9a24a62fd57559c203e67dd91468cdf63e7
Sebastian Jarosch <sjarosch@psanagpu101.pcdsn>
2017-11-30 19:53:12 -0800

    test

--- ana_microtof.py
+++ ana_microtof.py
@@ -12,25 +12,46 @@ from skbeam.core.accumulators.histogram import Histogram
 from FEEGasProcessing import FEEGasProcessor
 from psmon.plots import XYPlot,Image,Hist,MultiPlot
 from psmon import publish
+#publish.init(post=12304)
 
-
-ds = DataSource("exp=AMO/amolr2516:run=42:smd:dir=/reg/d/psdm/amo/amolr2516/xtc:live")
+run = 42
+ds = DataSource("exp=AMO/amolr2516:run=%d:smd:dir=/reg/d/psdm/amo/amolr2516/xtc:live" % run)
 procFEE=FEEGasProcessor()
 MINITOF_det = Detector('ACQ1')
 EBEAM_det = Detector('EBeam')
 
 numbins_L3Energy=100
-minhistlim_L3Energy=3300
-maxhistlim_L3Energy=3400
+minhistlim_L3Energy=3350
+maxhistlim_L3Energy=3450
 
 numbins_L3EnergyWeighted=100
-minhistlim_L3EnergyWeighted=3300
-maxhistlim_L3EnergyWeighted=3400
+minhistlim_L3EnergyWeighted=minhistlim_L3Energy
+maxhistlim_L3EnergyWeighted=maxhistlim_L3Energy
 
 hist_L3Energy=Histogram((numbins_L3Energy,minhistlim_L3Energy,maxhistlim_L3Energy))
 hist_L3EnergyWeighted=Histogram((numbins_L3EnergyWeighted,minhistlim_L3EnergyWeighted,maxhistlim_L3EnergyWeighted))
+hist_L3EnergyWeighted_unnorm=Histogram((numbins_L3EnergyWeighted,minhistlim_L3EnergyWeighted,maxhistlim_L3EnergyWeighted))
 
+vals = []
+ct = 0
 for nevt, evt in enumerate(ds.events()):
+    FEE_shot_energy = procFEE.ShotEnergy(evt)
+    if FEE_shot_energy is None:
+        continue
+    if nevt%50==0:
+        ct += 1
+        vals.append(FEE_shot_energy)
+    if ct > 50:
+        print "done"
+        break
+vals_std = np.asarray(vals).std()
+vals_mean = np.asarray(vals).mean()
+threshold = vals_mean - 2*vals_std
+print "threshold: %f, mean: %f, std: %f" % (threshold,vals_mean,vals_std)
+
+ds = DataSource("exp=AMO/amolr2516:run=%d:smd:dir=/reg/d/psdm/amo/amolr2516/xtc:live" % run)
+for nevt, evt in enumerate(ds.events()):
+
     wf= MINITOF_det.waveform(evt)
     #wf_time = MINITOF_det.wftime(evt)
     EBEAM = EBEAM_det.get(evt)
@@ -39,16 +60,34 @@ for nevt, evt in enumerate(ds.events()):
     EBEAM_beam_energy = EBEAM.ebeamL3Energy()  # beam energy in MeV
     FEE_shot_energy = procFEE.ShotEnergy(evt)
     #print FEE_shot_energy
-    if FEE_shot_energy is None:
+
+    if FEE_shot_energy is None or FEE_shot_energy<threshold:
         continue
-        
     hist_L3Energy.fill(EBEAM_beam_energy)
     hist_L3EnergyWeighted.fill([EBEAM_beam_energy], weights=[np.abs(wf[1][:10000].sum())/FEE_shot_energy])
-    
-    if nevt > 1000:
-        break
-    #break
-vShotsPerL3Energy=hist_L3EnergyWeighted/np.float(hist_L3Energy.values)
+    hist_L3EnergyWeighted_unnorm.fill([EBEAM_beam_energy], weights=[np.abs(wf[1][:10000].sum())])
+    if nevt%500==0:
+        print nevt
+        multi = MultiPlot(nevt,'MULTI')
+        vShotsPerL3Energy=np.nan_to_num(hist_L3EnergyWeighted.values/hist_L3Energy.values)
+        L3_weighted_plot = XYPlot(nevt, 'L3', hist_L3EnergyWeighted.centers[0], vShotsPerL3Energy)
+        #publish.send('L3', L3_weighted_plot)
+        multi.add(L3_weighted_plot)
+        vShotsPerL3Energy_un=np.nan_to_num(hist_L3EnergyWeighted_unnorm.values/hist_L3Energy.values)
+        L3_weighted_plot_un = XYPlot(nevt, 'L3UN', hist_L3EnergyWeighted.centers[0], vShotsPerL3Energy_un)
+        #publish.send('L3UN', L3_weighted_plot_un)
+        multi.add(L3_weighted_plot_un)
+        publish.send('MULTI',multi)
+        filename = "../npz/ana_itof_run_%d" %run
+        np.savez(filename,vShotsPerL3Energy=vShotsPerL3Energy,vShotsPerL3Energy_un=vShotsPerL3Energy_un,hist_L3Energy_centers=hist_L3EnergyWeighted.centers[0])
 
-L3_weighted_plot = XYPlot(nevt, 'L3', hist_L3EnergyWeighted.centers[0], hist_L3EnergyWeighted.values)
+    #break
+vShotsPerL3Energy=np.nan_to_num(hist_L3EnergyWeighted.values/hist_L3Energy.values)
+
+print "tset"
+
+L3_weighted_plot = XYPlot(nevt, 'L3', hist_L3EnergyWeighted.centers[0], vShotsPerL3Energy)
 publish.send('L3', L3_weighted_plot)
+time.sleep(0.5)
+#while 1==1:
+ #   x = 1
